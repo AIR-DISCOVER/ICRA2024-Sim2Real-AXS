defecc5f8de30d5af119d9dd084d7832598cbdd5
TiAmo_luo <117192954+hadliner@users.noreply.github.com>
2024-01-06 20:31:56 +0800

    Update run_baseline.sh

--- scripts/run_baseline.sh
+++ scripts/run_baseline.sh
@@ -1,12 +1,12 @@
 sudo docker rm -f baseline
 
-sudo docker run -it --name baseline --network=host \ 
-    --gpus all \ 
-    --privileged \ 
-    --device=/dev/ttyUSB0 \ 
-    --device=/dev/input/js0 \ 
-    -v /tmp/.X11-unix:/tmp/.X11-unix \ 
-    -e DISPLAY=$DISPLAY \ 
-    -v $HOME/Desktop/shared:/shared \ 
-    jieyitsinghuawx/icra2024-sim2real-axs-baseline:first_image \ 
+sudo docker run -it --name baseline --network=host \
+    --gpus all \
+    --privileged \
+    --device=/dev/ttyUSB0 \
+    --device=/dev/input/js0 \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -e DISPLAY=$DISPLAY \
+    -v $HOME/Desktop/shared:/shared \
+    jieyitsinghuawx/icra2024-sim2real-axs-baseline:update_baseline \
     /bin/bash
